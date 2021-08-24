58397b26225b5a78b07233108b994de7e72b7c3e
Avila Esteban <40841825+CQaS@users.noreply.github.com>
2021-08-24 17:31:50 -0300

    x

--- app/src/main/java/com/example/tp1usbcall/MainActivity.java
+++ app/src/main/java/com/example/tp1usbcall/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.tp1usbcall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDeviceConnection;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private UsbDeviceConnection USBcon;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},1000);
+        }
     }
-
-    //vemos
-
 }
