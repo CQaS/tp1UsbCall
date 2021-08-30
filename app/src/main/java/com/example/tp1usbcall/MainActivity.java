ba7ad84da36735d130deb9e26cd032b0751ce4bc
Avila Esteban <40841825+CQaS@users.noreply.github.com>
2021-08-30 14:23:11 -0300

    areglos

--- app/src/main/java/com/example/tp1usbcall/MainActivity.java
+++ app/src/main/java/com/example/tp1usbcall/MainActivity.java
@@ -19,12 +19,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
         && checkSelfPermission(Manifest.permission.CALL_PHONE)
         != PackageManager.PERMISSION_GRANTED)
         {
             requestPermissions(new String[]{ Manifest.permission.CALL_PHONE },1000);
-        }*/
+        }
     }
 
     @Override
